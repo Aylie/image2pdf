4e64c50db69ba03d028860e92646d435de129282
aylie_chou <aylie_chou@openfind.com.tw>
2016-10-17 03:59:50 -0700

    fit multiple image in one page

--- demo/pdfapi2.pl
+++ demo/pdfapi2.pl
@@ -3,11 +3,42 @@ use warnings;
 use PDF::API2;
 use Getopt::Long;
 
-# parse arguments
+sub GetPos
+{
+	my ($imgNum, $imgCnt, $blockWidth, $blockHeight) = @_
+	my $type = ($imgNum == 9)? ($imgNum / 3) : ($imgNum / 2);
+	my $crop = $imgNum / $type;
+	
+	$imgCnt = $imgCnt % $imgNum;
+	my $xpos = ($imgCnt % $type) * $bloickWidth;
+	my $ypos = ($crop - $imgCnt / $crop) * $blockHeight;
+
+	return ($xpos, $ypos);
+}
+
+sub GetSize
+{
+	my ($imgNum, $docWidth, $docHeight) = @_
+	my $type = ($imgNum == 9)? ($imgNum / 3) : ($imgNum / 2);
+	my $crop = $imgNum / $type;
+	
+	if (1 == $imgNum)
+		return ($docWidth, $docHeight);
+
+	$imgWidth = $docHeight / $type;
+	$imgHeight = $docWidth / $crop;
+	
+	return ($imgWidth, $imgHeight);
+}
+
+
+
+# define arguments
 my $imgPerPage = 1;
 my $pathIn = "./image/ppt/png/";
 my $pathOut = "./pdfapi2_png.pdf";
 
+# parse arguments
 GetOptions(
 	"img/page=i"	=> \$imgPerPage,	#int
 	"input=s"	=> \$pathIn,		#string
@@ -18,12 +49,12 @@ GetOptions(
 my $pdf = PDF::API2->new();
 
 # get a4 size
-#my @size = $pdf->get_page_size('A4');
-#print @size."\n";
 #my $a4page = $pdf->page();
 #$a4page->mediabox('A4');
 my $a4Width = 595;
 my $a4Height = 842;
+my $imgCnt = -1;
+my ($imgWidth, $imgHeight) = GetSize($imgPerPage, $a4Width, $a4Height);
 
 # open directory
 opendir(DIR, $pathIn) or die "Error : open $pathIn fail\n";
@@ -34,35 +65,38 @@ while (my $image = readdir DIR) {
 
 	# load image
 	my $png = $pdf->image_png($path.$image);
+	$imgCnt++;
 
-	# count scaling
-	my $imgWidth = $png->width();
-	my $imgHeight = $png->height();
+#	# count scaling
+#	my $imgWidth = $png->width();
+#	my $imgHeight = $png->height();
 #	print "width = $width\nheight = $height\n";
 	
-	my $scale = 0;
-	my $docWidth = $a4Width;
-	my $docHeight = $a4Height;
+#	my $scale = 0;
+#	my $docWidth = $a4Width;
+#	my $docHeight = $a4Height;
 	
-	# Add a page
-	my $page = $pdf->page();
+	if (0 == (imgCnt % $imgPerPage)) {
+		# Add a page
+		my $page = $pdf->page();
 
-	if ($imgWidth > $imgHeight) {
-		$docWidth = $a4Height;
-		$docHeight = $a4Width;
+		# Set page size
+		$page->mediabox($a4Width, $a4Height);
 	}
-	# Set page size
-	$page->mediabox($docWidth, $docHeight);
 
-	# calculate scaling
-	$scale = ($docWidth/$imgWidth) < ($docHeight/$imgHeight) ? $docWidth/$imgWidth : $docHeight/$imgHeight;
+#	if ($imgWidth > $imgHeight) {
+#		$docWidth = $a4Height;
+#		$docHeight = $a4Width;
+#	}
+
+#	# calculate scaling
+#	$scale = ($docWidth/$imgWidth) < ($docHeight/$imgHeight) ? $docWidth/$imgWidth : $docHeight/$imgHeight;
 	
-	$imgWidth *= $scale;
-	$imgHeight *= $scale;
+#	$imgWidth *= $scale;
+#	$imgHeight *= $scale;
 
 	# calculate pos
-	my $xpos = ($docWidth - $imgWidth) / 2;
-	my $ypos = ($docHeight - $imgHeight) / 2;
+	my ($xpos, $ypos) = GetPos()
 
 	# add image to pdf
 	my $gfx = $page->gfx;
